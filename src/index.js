98283156639c21288c26d2a3a85757320866aff6
Julia Grigorchuk <julia@jaybirdgroup.com>
2017-01-01 19:00:46 +0200

    Initiate firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
 import reactTapEventPlugin from 'react-tap-event-plugin';
+import {
+  initializeApp
+} from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyCCYaZZ623P_pQu276vuGf761kZk_EhOBI",
+  authDomain: "todo-85f3f.firebaseapp.com",
+  databaseURL: "http://todo-85f3f.firebaseio.com",
+  storageBucket: "todo-85f3f.appspot.com",
+  messagingSenderId: "1034314432668"
+};
+
+initializeApp(config);
+
 reactTapEventPlugin();
 
 ReactDOM.render(
