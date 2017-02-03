e2f812eaa52c17c027d63d696568501c59423356
Julia Grigorchuk <julia@jaybirdgroup.com>
2017-02-03 22:05:11 +0200

    Add test for task list showing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from '../app/App';
 import { database } from 'firebase';
 import {
@@ -19,4 +20,13 @@ initializeApp(config);
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders tasks list', () => {
+  const component = renderer.create(
+    <div className="TaskList"></div>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
