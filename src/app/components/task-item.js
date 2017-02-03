42dffb735aa3952cf8f978bf175e63795c56181d
Julia Grigorchuk <julia@jaybirdgroup.com>
2017-02-03 19:27:49 +0200

    Make edit without aditional input on start

--- src/app/components/task-item.js
+++ src/app/components/task-item.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Checkbox from 'material-ui/Checkbox';
 import FlatButton from 'material-ui/FlatButton';
 
 class TaskItem extends React.Component {
-  openInput( name ) {
+  openInput( name, id ) {
     console.log('Input shoud be shown here');
+    let nodeId = 'task-name-' + id.toString();
+    ReactDOM.render(
+      <input type="text"
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {this.props.updateTaskName(e, id)}}
+        }
+        defaultValue={name}
+        onBlur={(e) => this.props.updateTaskName(e, id)} />,
+      document.getElementById(nodeId));
   }
 
   render() {
@@ -15,12 +25,13 @@ class TaskItem extends React.Component {
       deleteTask,
     } = this.props;
 
+    let taskItemNodeId = "task-name-" + taskItem.id.toString();
+
 		return (
       <div className="TaskItem" style={{display: 'flex', justifyContent: 'flex-start'}}>
         <Checkbox onCheck={() => toggleCompleted(taskItem.id)} checked={taskItem.isCompleted} style={{width: 'auto'}} />
         <div>
-          <span onClick={() => this.openInput(taskItem.name)}>{taskItem.name}</span>
-          <input type="text" onChange={(e) => updateTaskName(e, taskItem.id)} />
+          <span onClick={() => this.openInput(taskItem.name, taskItem.id)} id={taskItemNodeId}>{taskItem.name}</span>
           <FlatButton label="Delete Task" secondary={true} onTouchTap={() => deleteTask(taskItem.id)} />
         </div>
       </div>
