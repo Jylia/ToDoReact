964a6f0fa28fe5cb6ed89c1cfb7f3c0e344d1cc5
Julia Grigorchuk <grigorchukj@gmail.com>
2017-02-07 19:05:37 +0200

    Remove console.log

--- src/app/components/task-item.js
+++ src/app/components/task-item.js
@@ -5,7 +5,7 @@ import FlatButton from 'material-ui/FlatButton';
 
 class TaskItem extends React.Component {
   openInput( name, id ) {
-    console.log('Input shoud be shown here');
+
     let nodeId = 'task-name-' + id.toString();
     ReactDOM.render(
       <input type="text"
