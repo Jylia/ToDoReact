a2ff1355ae5fc364df506f312d65a16dc657cc76
Julia Grigorchuk <grigorchukj@gmail.com>
2017-02-20 16:53:33 +0200

    Remove firedux

--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import FireduxInstance from './fireduxSettings';
 import todo from './app/todoReducer';
 import filter from './app/filterReducer';
 
 const mainReducer = combineReducers({
   todo,
-  filter,
-  firedux: FireduxInstance.reducer()
+  filter
 });
 
 export default mainReducer;
