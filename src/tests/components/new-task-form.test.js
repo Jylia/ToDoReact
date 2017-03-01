7c9cc6d951925140ef77af9db06d3fa3c4c71722
Julia Grigorchuk <grigorchukj@gmail.com>
2017-03-01 02:59:30 +0200

    Add test for new task form

---
+++ src/tests/components/new-task-form.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+import { NewTaskForm } from '../../app/components/new-task-form';
+
+describe('components', () => {
+  describe('NewTaskForm', () => {
+    test(`NewTaskForm should be showed`, () => {
+
+      const component = shallow(
+          <NewTaskForm />
+      );
+      
+      expect(component.find('form').exists()).toBe(true);
+      expect(component.find(RaisedButton).exists()).toBe(true);
+      expect(component.find(TextField).exists()).toBe(true);
+    });
+  });
+});
