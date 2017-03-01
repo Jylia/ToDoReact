a9e2cfe8b63a3193c9678865d4bb7bf63e5b5de5
Julia Grigorchuk <grigorchukj@gmail.com>
2017-03-01 02:55:29 +0200

    Add test for Filter

---
+++ src/tests/components/filter.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import RaisedButton from 'material-ui/RaisedButton';
+
+import { Filter } from '../../app/components/filter';
+
+describe('components', () => {
+  describe('Filter', () => {
+    test(`Filter should be showed`, () => {
+
+      const component = shallow(
+          <Filter/>
+      );
+      
+      expect(component.find(RaisedButton).exists()).toBe(true);
+      expect(component.find(RaisedButton).length).toBe(3);
+    });
+  });
+});
